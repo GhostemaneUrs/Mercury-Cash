695a33ed6f3681543f215e2b7315b7396a66f61d
Andres Jaramillo <JaramilloG1999@outlook.com>
2022-10-13 14:53:49 -0500

    feat: update structure root reducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,11 +1,9 @@
 import { combineReducers } from '@reduxjs/toolkit'
 import users from './slices/users'
-import settings from './slices/settings'
 import countries from './slices/countries'
 
 const rootReducer = combineReducers({
   users,
-  settings,
   countries,
 })
 
